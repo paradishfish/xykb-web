f0211a230505335989c7f577ee5ab3aa53f48dfb
paradishfish <382634149@qq.com>
2021-04-27 18:32:58 +0800

    element

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,7 +5,14 @@
       密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
       <br><br>
       <button v-on:click="login">登录</button>
-  </div>
+      <el-select v-model="value" placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
 </template>
 
 <script>
@@ -18,8 +25,26 @@
           username: '',
           password: ''
         },
-        responseResult: []
-      }
+        responseResult: [],
+        msg:'welcome to vue app',
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: ''
+        }
     },
     methods: {
       login () {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import * as Echarts from 'echarts'
 Vue.prototype.$echarts = Echarts
 
@@ -11,7 +13,7 @@ axios.defaults.baseURL = 'http://localhost:8443/api'
 Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
